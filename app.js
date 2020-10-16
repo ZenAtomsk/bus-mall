d69b1a59b4e13cc3fc344edbe316d4242c1706d4
ZenAtomsk <ricardo.b3788@gmail.com>
2020-10-15 22:34:00 -0700

    local storage, chart names and graph names duplicating. random image slot not showing up when refreshed

--- app.js
+++ app.js
@@ -15,38 +15,51 @@ var yChartData = [];
 // console.log('2. my all products array as JSON:', stringifyProducts);
 
 
-function Product(filepath, productName){
+function Product(filepath, productName, views = 0, votes = 0){
   this.filepath = filepath;
   this.name = productName;
-  this.views = 0;
-  this.votes = 0;
+  this.views = views;
+  this.votes = votes;
   // xChartProductName.push(name);
   xChartProductName.push(this.name);
   allProducts.push(this);
 }
 
+if(!localStorage.getItem('products')){
+  productList();
+} else {
+  var productsFromLocalStorage = localStorage.getItem('products');
+  var parsedProducts = JSON.parse(productsFromLocalStorage);
 
+  for(var i = 0; i< parsedProducts.length; i++){
+    new Product(parsedProducts[i].src, parsedProducts[i].name, parsedProducts[i].views, parsedProducts[i].votes);
+  }
+  productList();
+}
 
-new Product('img/bag.jpg', 'bag');
-new Product('img/banana.jpg', 'banana');
-new Product('img/bathroom.jpg', 'bathroom');
-new Product('img/boots.jpg', 'boots');
-new Product('img/breakfast.jpg', 'breakfast');
-new Product('img/bubblegum.jpg', 'bubblegum');
-new Product('img/chair.jpg', 'chair');
-new Product('img/cthulhu.jpg', 'cthulhu');
-new Product('img/dog-duck.jpg', 'dog-duck');
-new Product('img/dragon.jpg', 'dragon');
-new Product('img/pen.jpg', 'pen');
-new Product('img/pet-sweep.jpg', 'pet-sweep');
-new Product('img/scissors.jpg', 'scissors');
-new Product('img/shark.jpg', 'shark');
-new Product('img/sweep.png', 'sweep');
-new Product('img/tauntaun.jpg', 'tauntaun');
-new Product('img/unicorn.jpg', 'unicorn');
-new Product('img/usb.gif', 'usb');
-new Product('img/water-can.jpg', 'water-can');
-new Product('img/wine-glass.jpg', 'wine-glass');
+function productList(){
+  new Product('img/bag.jpg', 'bag');
+  new Product('img/banana.jpg', 'banana');
+  new Product('img/bathroom.jpg', 'bathroom');
+  new Product('img/boots.jpg', 'boots');
+  new Product('img/breakfast.jpg', 'breakfast');
+  new Product('img/bubblegum.jpg', 'bubblegum');
+  new Product('img/chair.jpg', 'chair');
+  new Product('img/cthulhu.jpg', 'cthulhu');
+  new Product('img/dog-duck.jpg', 'dog-duck');
+  new Product('img/dragon.jpg', 'dragon');
+  new Product('img/pen.jpg', 'pen');
+  new Product('img/pet-sweep.jpg', 'pet-sweep');
+  new Product('img/scissors.jpg', 'scissors');
+  new Product('img/shark.jpg', 'shark');
+  new Product('img/sweep.png', 'sweep');
+  new Product('img/tauntaun.jpg', 'tauntaun');
+  new Product('img/unicorn.jpg', 'unicorn');
+  new Product('img/usb.gif', 'usb');
+  new Product('img/water-can.jpg', 'water-can');
+  new Product('img/wine-glass.jpg', 'wine-glass');
+}
+
 
 function render(imageElement){
 
@@ -73,8 +86,8 @@ function getRandomNumber(min, max){
 }
 
 function handleClick(event){
-  var productsFromLocalStorage = localStorage.getItem('stringifyProducts');
-  var parsedProducts = JSON.parse(productsFromLocalStorage);
+  // var productsFromLocalStorage = localStorage.getItem('stringifyProducts');
+  // var parsedProducts = JSON.parse(productsFromLocalStorage);
 
   var chosenProduct = event.target.title;
 
@@ -96,15 +109,18 @@ function handleClick(event){
     document.getElementById('image-container').removeEventListener('click', handleClick);
     //display results
     var resultsElement = document.getElementById('results');
+
     for(var j = 0; j < allProducts.length; j++){
+
       var liElement = document.createElement('li');
       liElement.textContent = `${allProducts[j].name} was seen ${allProducts[j].views} times.`;
       resultsElement.appendChild(liElement);
       yChartData.push(allProducts[j].votes);
       // console.log(yChartData);
       renderChart();
-      var stringifyProducts = JSON.stringify(allProducts[j].votes);
-      console.log('2. my all products array as JSON:', stringifyProducts);
+
+      var stringifyProducts = JSON.stringify(allProducts);
+      // console.log('2. my all products array as JSON:', stringifyProducts);
       localStorage.setItem('products', stringifyProducts);
     }
     // var stringifyProducts = JSON.stringify(allProducts);
@@ -115,7 +131,7 @@ function handleClick(event){
 //can also be called w/out the global var with document.getElementById('image-container').addEventListener('click', handleClick)
 document.getElementById('image-container').addEventListener('click', handleClick);
 
-
+// productList();
 render(imageOneElement);
 render(imageTwoElement);
 render(imageThreeElement);
